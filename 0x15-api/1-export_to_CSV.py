439f4df608023b6b3d71145db8b806b07bd18b6c
dotunoflagos <adedotun_balogun@yahoo.com>
2023-02-09 15:22:13 +0100

    aPI

--- 0x15-api/1-export_to_CSV.py
+++ 0x15-api/1-export_to_CSV.py
@@ -1,30 +1,38 @@
 #!/usr/bin/python3
-'''A script that gathers data from an API and exports it to a CSV file.
-'''
-import re
-import requests
-import sys
+"""
+Return information for a given employee about his/her TODO list progress
+"""
+if __name__ == "__main__":
+    import csv
+    import requests
+    import sys
 
+    DONE_TASKS = 0
+    ALL_TASKS = 0
+    csvList = []
 
-API_URL = 'https://jsonplaceholder.typicode.com'
-'''The API's URL.'''
+    URL_FOR_USERS = 'https://jsonplaceholder.typicode.com/users/{0}'.\
+        format(sys.argv[1])
+    URL_FOR_TODOS = 'https://jsonplaceholder.typicode.com/todos'
+    r_for_users = requests.get(URL_FOR_USERS)
+    r_for_todos = requests.get(URL_FOR_TODOS)
 
-
-if __name__ == '__main__':
-    if len(sys.argv) > 1:
-        if re.fullmatch(r'\d+', sys.argv[1]):
-            id = int(sys.argv[1])
-            user_res = requests.get('{}/users/{}'.format(API_URL, id)).json()
-            todos_res = requests.get('{}/todos'.format(API_URL)).json()
-            user_name = user_res.get('username')
-            todos = list(filter(lambda x: x.get('userId') == id, todos_res))
-            with open('{}.csv'.format(id), 'w') as file:
-                for todo in todos:
-                    file.write(
-                        '"{}","{}","{}","{}"\n'.format(
-                            id,
-                            user_name,
-                            todo.get('completed'),
-                            todo.get('title')
-                        )
-                    )
+    name = r_for_users.json().get('name')
+    user_name = r_for_users.json().get('username')
+    todos = r_for_todos.json()
+    for todo in todos:
+        if todo.get('userId') == int(sys.argv[1]):
+            subcsvList = []
+        if todo.get('userId') == int(sys.argv[1]):
+            ALL_TASKS += 1
+            subcsvList.append(todo.get('userId'))
+            subcsvList.append(user_name)
+            subcsvList.append(todo.get('completed'))
+            subcsvList.append(todo.get('title'))
+            csvList.append(subcsvList)
+        if (todo.get('userId') == int(sys.argv[1]))\
+                and (todo.get('completed')):
+            DONE_TASKS += 1
+    with open(sys.argv[1] + '.csv', 'w+', newline='') as f:
+        writer = csv.writer(f, quoting=csv.QUOTE_ALL)
+        writer.writerows(csvList)
